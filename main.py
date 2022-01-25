69c64a28389256c2546bf239226b1852dee5e2cc
wolls <wollscheidtf@gmail.com>
2022-01-25 16:09:08 +0100

    code

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 import time
-
 import pygame
 pygame.init()
+
+#Classe joueur #
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.health = 3
+        self.max_health = 3
+        self.attack = 10
+        self.velocity = 5
+        self.image = pygame.image.load('assets/vaisseaux/playerOne.png')
+        self.image.get_rect()
+
 
 # Générer une fenêtre de jeu #
 pygame.display.set_caption("SpaceShoot")
 screen = pygame.display.set_mode((400, 600))
-
 #background#
 background = pygame.image.load('assets/fond/backgroundSpace.png')
+
+#Chargement de notre joueur #
+player = Player()
 
 # Boucle jeu #
 running = True
 while running :
     #appliquer arrière plan #
     screen.blit(background,(0,0))
+    #Appliquer image de notre joueur#
+    screen.blit(player.image, (127,500))
     #mettre à jour l'écran #
     pygame.display.flip()
 
