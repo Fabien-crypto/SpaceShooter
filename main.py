79fc3d31e7977168b94f8f67fe2886e80ae8adf2
wolls <wollscheidtf@gmail.com>
2022-01-28 22:17:21 +0100

    Validation

--- main.py
+++ main.py
@@ -2,48 +2,61 @@ import time
 import pygame
 from game import Game
 pygame.init()
+
+#Temps du jeu #
+clock = pygame.time.Clock()
 # Générer une fenêtre de jeu #
 pygame.display.set_caption("SpaceShoot")
 screen = pygame.display.set_mode((400, 600))
 #background#
-background = pygame.image.load('assets/fond/backgroundSpace.png')
+background = pygame.image.load('assets/fond/frameBackground.png')
+background = pygame.transform.scale(background,(400,600))
+y_background = 0
 
 #Chargement de notre jeu#
 game = Game()
 # Boucle jeu #
 running = True
+pygame.key.set_repeat
 while running :
-    #appliquer arrière plan #
-    screen.blit(background,(0,0))
+    #appliquer arrière plan et défilement#
+    y_background += 1
+    if y_background < 600 :
+        screen.blit(background,(0,y_background))
+        screen.blit(background, (0, y_background-600))
+    else :
+        y_background = 0
+        screen.blit(background, (0, y_background))
     #Appliquer image de notre joueur#
     screen.blit(game.player.image, game.player.rect)
+    clock.tick(130)
     #récupérer tout les projectiles du joueur #
     for projectile in game.player.all_projectiles :
         projectile.move()
-    #Appliquer l'ensemble de mon grp de projectiles#
-    game.player.all_projectiles.draw(screen) 
+    #Appliquer l'ensemble de mon grp de projectiles en les dessinant#
+    game.player.all_projectiles.draw(screen)
 
-    #vérifier si le joueur souhaite bouger#
+    # vérifier si le joueur souhaite bouger ou tirer#
+    if game.pressed.get(pygame.K_SPACE):
+        game.player.launch_projectile()
     if game.pressed.get(pygame.K_DOWN) and game.player.rect.y + (game.player.rect.height) < screen.get_height():
         game.player.move_down()
-        if game.pressed.get(pygame.K_SPACE)  :
+        if game.pressed.get(pygame.K_SPACE):
             game.player.launch_projectile()
-    elif game.pressed.get(pygame.K_UP) and game.player.rect.y > 0:
+    if game.pressed.get(pygame.K_UP) and game.player.rect.y > 0:
         game.player.move_up()
-        if game.pressed.get(pygame.K_SPACE)  :
+        if game.pressed.get(pygame.K_SPACE):
             game.player.launch_projectile()
-    elif game.pressed.get(pygame.K_RIGHT) and game.player.rect.x + (game.player.rect.width)/2 < screen.get_width() :
+    if game.pressed.get(pygame.K_RIGHT) and game.player.rect.x + (game.player.rect.width) / 2 < screen.get_width():
         game.player.move_right()
-        if game.pressed.get(pygame.K_SPACE) :
+        if game.pressed.get(pygame.K_SPACE):
             game.player.launch_projectile()
-    elif game.pressed.get(pygame.K_LEFT) and game.player.rect.x > 0-(game.player.rect.width)/2 :
+    if game.pressed.get(pygame.K_LEFT) and game.player.rect.x > 0 - (game.player.rect.width) / 2:
         game.player.move_left()
-        if game.pressed.get(pygame.K_SPACE) :
+        if game.pressed.get(pygame.K_SPACE):
             game.player.launch_projectile()
-    else:
-        game.player.no_move()
-        if game.pressed.get(pygame.K_SPACE) :
-            game.player.launch_projectile()
+
+
     #mettre à jour l'écran  #
     pygame.display.flip()
     #fermeture du jeu#
@@ -52,7 +65,7 @@ while running :
             running = False
             pygame.quit()
             print("fermeture du jeu")
-        elif event.type == pygame.KEYDOWN :
+        if event.type == pygame.KEYDOWN :
             game.pressed[event.key]=True
         elif event.type == pygame.KEYUP :
             game.pressed[event.key] = False
--- player.py
+++ player.py
@@ -7,33 +7,37 @@ class Player(pygame.sprite.Sprite):
         self.health = 3
         self.max_health = 3
         self.attack = 10
-        self.velocity = 2.6
+        self.velocity = 3
         self.all_projectiles = pygame.sprite.Group()
-        self.image = pygame.image.load('assets/vaisseaux/_0000_Layer-1.png')
+        self.image = pygame.image.load('assets/vaisseaux/nomove.png')
         self.image = pygame.transform.scale(self.image, (80,80))
         self.rect = self.image.get_rect()
         self.rect.x = 129
         self.rect.y = 500
+        self.shoot_delay = 150
+        self.last_shoot =  pygame.time.get_ticks()
     def launch_projectile(self):
         #Création nouvelle instance du projectile#
-        self.all_projectiles.add(Projectile(self))
-
+        now = pygame.time.get_ticks()
+        if now - self.last_shoot > self.shoot_delay:
+            self.all_projectiles.add(Projectile(self))
+            self.last_shoot = now
     def move_right(self):
         self.rect.x += self.velocity
-        self.image = pygame.image.load('assets/vaisseaux/_0003_Layer-3.png')
-        self.image = pygame.transform.scale(self.image, (80,80))
+        self.image = pygame.image.load('assets/vaisseaux/right.png')
+        self.image = pygame.transform.scale(self.image, (80, 80))
     def move_left(self):
         self.rect.x -= self.velocity
-        self.image = pygame.image.load('assets/vaisseaux/_0001_Layer-2.png')
+        self.image = pygame.image.load('assets/vaisseaux/left.png')
         self.image = pygame.transform.scale(self.image, (80,80))
     def move_up(self):
         self.rect.y -= self.velocity
-        self.image = pygame.image.load('assets/vaisseaux/_0000_Layer-1.png')
+        self.image = pygame.image.load('assets/vaisseaux/nomove.png')
         self.image = pygame.transform.scale(self.image, (80,80))
     def move_down(self):
         self.rect.y += self.velocity
-        self.image = pygame.image.load('assets/vaisseaux/_0000_Layer-1.png')
+        self.image = pygame.image.load('assets/vaisseaux/nomove.png')
         self.image = pygame.transform.scale(self.image, (80,80))
     def no_move(self):
-        self.image = pygame.image.load('assets/vaisseaux/_0000_Layer-1.png')
+        self.image = pygame.image.load('assets/vaisseaux/nomove.png')
         self.image = pygame.transform.scale(self.image, (80,80))
