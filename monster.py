7b86ba69d68bb7e21891a3e3894df0e40843fc40
Antoniu2a <michelozzi2b@outlook.com>
2022-03-21 13:59:05 +0100

    v3

---
+++ monster.py
@@ -0,0 +1,44 @@
+import pygame
+from random import randint
+from laser_ennemie import Laser_ennemie
+import time
+from explosion import Explosion
+
+#Classe du monstre#
+class Monster(pygame.sprite.Sprite):
+    def __init__(self, game):
+        super().__init__()
+        self.game = game
+        self.explosion = pygame.mixer.Sound('sounds/ennemy_explosion.aiff')
+        self.explosion.set_volume(0.5)
+        self.health = 30
+        self.max_health = 30
+        self.attack = 10
+        self.velocity = 1.5
+        self.all_laser = pygame.sprite.Group()
+        self.image = pygame.image.load("assets/vaisseaux/ennemies/enemy-01/nomove_1.png")
+        self.image = pygame.transform.scale(self.image, (60,60))
+        self.rect = self.image.get_rect()
+        self.rect.x = randint(-15,360)
+        self.rect.y = -10
+        self.delay = 90
+ 
+    def damage(self, amount) :
+        self.health -= amount
+        if self.health <= 0:
+            explosion = Explosion(self.rect.centerx, self.rect.centery, 1)
+            self.game.explosion_group.add(explosion)
+            self.game.all_monsters.remove(self)
+            self.game.player.score += 5
+
+    def forward(self):
+        self.rect.y += self.velocity
+        if self.game.check_collision(self,self.game.all_players) :
+            self.game.player.damage(self.attack)
+            self.game.all_monsters.remove(self)
+
+    def launch_laser(self) :
+        self.all_laser.add(Laser_ennemie(self))
+
+
+
---
+++ projectile.py
@@ -0,0 +1,31 @@
+import pygame
+#classe projectile de notre vaisseau #
+class Projectile(pygame.sprite.Sprite) :
+    # Définir le constructeur de cette classe #
+    def __init__(self, player):
+        super().__init__()
+        soundObj = pygame.mixer.Sound('sounds/player_shoot.wav')
+        soundObj.set_volume(1.0)
+        soundObj.play()
+        self.velocity = 9
+        self.player = player
+        self.image = pygame.image.load('assets/missiles/PlayProjectile.png')
+        self.image = pygame.transform.scale(self.image, (20,20))
+        self.rect = self.image.get_rect()
+        self.rect.x = player.rect.x + (player.rect.width)/2 - self.image.get_width()/2
+        self.rect.y = player.rect.y
+
+    def remove(self):
+        self.player.all_projectiles.remove(self)
+
+    def move(self):
+        for monster in self.player.game.check_collision(self, self.player.game.all_monsters) :
+            self.remove()
+            monster.damage(self.player.attack)
+        if self.rect.y < -10 :
+            self.remove()
+        self.rect.y -= self.velocity
+            
+        
+
+
